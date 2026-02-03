bfb0f2257ae3d19f1076ed2c6164c1bb7c5330dc
yoan <joanmat@proton.me>
2026-02-03 16:24:30 -0500

    add favicon

--- src/components/head.php
+++ src/components/head.php
@@ -50,4 +50,11 @@ $config = [
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Google+Sans:opsz,wght@17..18,400..700&display=swap" rel="stylesheet">
 
+<link rel="icon" type="image/png" href="/src/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/src/favicon/favicon.svg" />
+<link rel="shortcut icon" href="/src/favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/src/favicon/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Recursos Prueba de Ingreso" />
+<link rel="manifest" href="/src/favicon/site.webmanifest" />
+
 </head>
